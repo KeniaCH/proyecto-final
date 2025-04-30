d2125d9102622d8d0c4b43ecac40b3851fe339a5
Kenia Castillo <keniacastillohdez@gmail.com>
2025-04-30 09:44:41 -0600

    clase 1

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+    public function firstUser(){
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'bjbkjb@gmail.com',
+            'password' => bcrypt('12345678'),
+            'id_branch' => 1,
+            'photo' => '',
+            'rol' => 'admin',
+            'status' => 'active',
+            'las_login' => '',
+        ]);
+        return "User created successfully";
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\UsersController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/first-user',[UsersController::class,'FirtUser']);
+
+Route::get('/',function(){
+    return view('modules.users.login');
+});
